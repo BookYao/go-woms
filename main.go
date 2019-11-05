fcaa88f66ffdbad36c269ff6a1a09bf206ee9656
BookYao <workfile_yao@163.com>
2019-11-05 17:30:24 +0800

    1. add go-woms main file

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"./eventmodule"
+)
+
+const (
+	Event_UP_Interface = "/woms/api/UpEvent.php"
+	Query_Single_Event = "/woms/api/QuerySingleEvent.php"
+	Event_Status_UpDate = "/woms/api/UpEventStatus.php"
+)
+
+func main() {
+	fmt.Println("WOMS Http Interface start...")
+
+	http.HandleFunc(Event_UP_Interface, eventmodule.EventUpload)
+	http.HandleFunc(Query_Single_Event, eventmodule.QuerySingleEvent)
+	http.HandleFunc(Event_Status_UpDate, eventmodule.EventStatusUpdate)
+
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		fmt.Println("Http Listen Error.", err.Error())
+		log.Fatal("ListenAndServe error:", err)
+	}
+}
